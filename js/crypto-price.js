fbf58c68dbd7027aef50d7f43413377512eccb52
Akli Reguig <akli.reguig@gmail.com>
2017-12-23 23:38:41 +0100

    Add filter by query param. x

--- js/crypto-price.js
+++ js/crypto-price.js
@@ -8,8 +8,26 @@
     }
   })
 
-  $.getJSON('https://areguig.github.io/crypto-price/currencies.json', function(data){ 
-    app.items=data.slice(0,100);;
+
+  var urlParams;
+  (window.onpopstate = function () {
+      var match,
+          pl     = /\+/g,  // Regex for replacing addition symbol with a space
+          search = /([^&=]+)=?([^&]*)/g,
+          decode = function (s) { return decodeURIComponent(s.replace(pl, " ")); },
+          query  = window.location.search.substring(1);
+  
+      urlParams = {};
+      while (match = search.exec(query))
+         urlParams[decode(match[1])] = decode(match[2]);
+  })();
+alert(urlParams)
+  $.getJSON('https://areguig.github.io/crypto-price/currencies.json', function(data){
+    if(urlParams.list && Array.isArray(JSON.parse(urlParams.list))){
+      app.items = data.filter( d => JSON.parse(urlParams.list).includes(d.slug))
+    } else {
+      app.items=data.slice(0,100);
+    }
   })
 
   new autoComplete({
