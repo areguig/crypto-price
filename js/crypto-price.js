5ae5ccd0557ac3fa88d690d4baa30c333a94cba9
Akli Reguig <akli.reguig@gmail.com>
2017-12-23 21:46:57 +0100

    Use local json for currencies listxY

--- js/crypto-price.js
+++ js/crypto-price.js
@@ -1,13 +1,6 @@
-var app = new Vue({
-    el: '#app',
-    data: {
-      message: 'Hello Vue!'
-    }
-  })
-
   new autoComplete({
     selector: 'input[id="search"]',
     source: function(term, response){
-        $.getJSON('https://files.coinmarketcap.com/generated/search/quick_search.json', { q: term }, function(data){ response(data.map(t => t.slug)); });
+        $.getJSON('https://areguig.github.io/crypto-price/currencies.json', { q: term }, function(data){ response(data.map(d=>d.name)); });
     }
 });
